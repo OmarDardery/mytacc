b5412be4c984aeaece4baf84f363964bfb0aaed0
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2025-05-14 00:45:53 +0300

    fixing deployment

--- mytacc/settings.py
+++ mytacc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1&+m%kyg5p$auy$#rg%mz@_u)-49j536k343a5^$6p6fap=un3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["mytacc.onrender.com"]
 
