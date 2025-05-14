65311102a5f15952bdf9181ef7dbf640f4f9da21
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2025-05-14 16:52:35 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 echo "📦 Installing Node dependencies..."
 cd templates/mainApp_react/liasu || exit
 npm install
-
+chmod +x ./node_modules/.bin/react-scripts
 echo "🏗️ Building React..."
 npm run build
